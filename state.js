575e2d6d11c655be5fb9dbac243d0d7421133c6f
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-16 11:20:11 -0700

    built an initial form of state

--- state.js
+++ state.js
@@ -4,6 +4,15 @@ const state = {};
 // initialize state, also used in test
 export function initialize() {
     // What is the initial shape of state?
+    state.hp = 10;
+    state.message = '';
+    state.type = ['monster', 'monster2', 'monster3'];
+    state.damage = [0, 0, 0, 1, 1, 1, 2, 2, 3];
+    state.monsters = [
+        { name: 'Heathcliff', health: 4, },
+        { name: 'Lloyd', health: 4, },
+        { name: 'Gilgamesh', health: 5, },
+    ];
     // For example:
     // state.game = null;
     // state.pastGames = [];
@@ -14,3 +23,9 @@ initialize();
 export default state;
 
 // export dispatch functions that modify state
+
+export function setMessage(message) {
+    state.message = message;
+}
+
+export function addMonster()
